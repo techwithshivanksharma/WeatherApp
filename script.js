e39b3a22700246ecb5e893cc9aace13e9add4753
Shivank Sharma <137379367+techwithshivanksharma@users.noreply.github.com>
2024-01-15 20:21:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey="90f427cccc3bbfea5939346c2b93d408";
+const apiKey="Your Api Key";
 const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox=document.querySelector('.search input');
